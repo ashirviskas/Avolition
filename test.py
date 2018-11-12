9dce45d7819d94f0ba4860c3aae48100868cf3e5
Taurintas Kudirka <taurisx@gmail.com>
2018-11-12 22:32:07 +0200

    Added EyeTribe cursor control for PC1 (melee warrior)

---
+++ test.py
@@ -0,0 +1,19 @@
+from peyetribe import EyeTribe
+import time
+
+tracker = EyeTribe(host="localhost", port=6555)
+tracker.connect()
+n = tracker.next()
+
+print("eT;dT;aT;Fix;State;Rwx;Rwy;Avx;Avy;LRwx;LRwy;LAvx;LAvy;LPSz;LCx;LCy;RRwx;RRwy;RAvx;RAvy;RPSz;RCx;RCy")
+
+tracker.pushmode()
+count = 0
+while count < 100:
+    n = tracker.next()
+    print(n)
+    count += 1
+
+tracker.pullmode()
+
+tracker.close()
